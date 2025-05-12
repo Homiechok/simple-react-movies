3595c0ec1d13585a6173012aa57051085c0a0ac9
Maksim Ushakov <jenadka@bk.ru>
2025-05-12 15:34:17 +0500

    feat: Create MovieCard component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,22 @@
+export default function MovieCard({ movie }) {
+  function handleClick() {
+    alert("Like");
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.id} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={handleClick}>
+            ♥
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
+}
