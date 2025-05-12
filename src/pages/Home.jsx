8788533aab27fc491390ca0ccd36c3eecb50be1b
Maksim Ushakov <jenadka@bk.ru>
2025-05-12 15:58:08 +0500

    feat: Add state for search-input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,31 @@
 import MovieCard from "../components/MovieCard.jsx";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "The Movie 1", release_date: 2020 },
     { id: 2, title: "The Movie 2", release_date: 2022 },
     { id: 3, title: "The Movie 3", release_date: 2024 },
   ];
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
       <form className="search-form" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Search..." className="search-input" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
         <button type="submit" className="search-button">
           Search
         </button>
